3decbd9c99ecf6d29ffc0efd3f596d57d24c0d35
Abismael Tapia <abismaeltapia@gmail.com>
2022-10-05 23:33:12 -0400

    Add div grid

---
+++ program/javascript.js
@@ -0,0 +1,11 @@
+const mainContainer = document.getElementById("container");
+
+function rowsAndCols (rows,cols) {
+    for (let i = 0; i < rows; i++) {
+        const box = document.createElement("div");
+        mainContainer.appendChild(caja);
+        box.classList.add("divs");
+        box.classList.add("flex");
+    }
+}
+rowsAndCols();
